c78940362531f58ebc545a8b7a40d832414b902b
josecsotomorales <josecsmorales@gmail.com>
2022-03-20 21:46:03 -0300

    Add application access outputs

--- container/outputs.tf
+++ container/outputs.tf
@@ -1,9 +1,3 @@
-# output "container_name" {
-#   value       = docker_container.container.name
-#   description = "The name of the container"
-# }
-
-# output "ip_address" {
-#   value       = [for i in docker_container.container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
-#   description = "The IP address and external port of the container"
-# }
+output "application_access" {
+  value = {for x in docker_container.app_container[*]: x.name => join(":", [x.ip_address], x.ports[*]["external"])}
+}
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,4 @@
-# output "container_name" {
-#   value       = module.container[*].container_name
-#   description = "The name of the container"
-# }
-
-# output "ip_address" {
-#   value       = flatten(module.container[*].ip_address)
-#   description = "The IP address and external port of the container"
-# }
+output "application_access" {
+  value = [for x in module.container[*]: x]
+  description = "The name and socket for each application"
+}
